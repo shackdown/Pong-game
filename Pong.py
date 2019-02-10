e84e2d9b2ae07e71dd0a65f5dc1cbf740eac1ca2
shackdown <heraudetc@gmail.com>
2019-02-10 10:40:52 +0100

    Create Pong.py
    Add Pong.py

---
+++ Pong.py
@@ -0,0 +1,68 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+
+#Paddle Be
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
+#Ball
+Ball = turtle.Turtle()
+Ball.speed(0)
+Ball.shape("square")
+Ball.color("white")
+Ball.penup()
+Ball.goto(0,0)
+
+
+#Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20 
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+#Keybord binding
+wn.listen()
+wn.onkey(paddle_a_up,"w")
+wn.onkey(paddle_a_down,"s")
+wn.onkey(paddle_b_up,"Up")
+wn.onkey(paddle_b_down,"Down")
+
+# Main game loop
+while True:
+    wn.update()
