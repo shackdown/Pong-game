d262c9511b7ae3c1178c5166054220cf97e73ce7
shackdown <heraudetc@gmail.com>
2019-02-10 13:46:20 +0100

    Scoring

--- Pong.py
+++ Pong.py
@@ -6,6 +6,9 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+#Score
+score_a = 0
+score_b = 0
 
 #Paddle A
 paddle_a = turtle.Turtle()
@@ -35,6 +38,15 @@ ball.penup()
 ball.goto(0,0)
 ball.dx = 2
 ball.dy = 2
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: {} / Player B: {}".format(score_a, score_b), align="center", font=("Courier",24, "normal"))
 
 
 #Function
@@ -86,10 +98,16 @@ while True:
     if ball.xcor()> 390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} / Player B: {}".format(score_a, score_b), align="center", font=("Courier",24, "normal"))
     
     if ball.xcor()<-390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} / Player B: {}".format(score_a, score_b), align="center", font=("Courier",24, "normal"))
 
     # Paddle and ball collision
     if (ball.xcor() > 340 and ball.xcor()<350) and (ball.ycor() < paddle_b.ycor()+40 and ball.ycor() > paddle_b.ycor() - 40):
